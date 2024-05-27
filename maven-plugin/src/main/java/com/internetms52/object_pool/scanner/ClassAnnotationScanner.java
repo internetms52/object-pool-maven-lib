eb33e74d0d22e92d190fc74eb8157d884cc0a7af
internetms52 <internetms52@gmail.com>
2024-05-27 10:02:35 +0800

    add object pool annotation scanner
    add object pool annotation scanner

---
+++ maven-plugin/src/main/java/com/internetms52/object_pool/scanner/ClassAnnotationScanner.java
@@ -0,0 +1,43 @@
+package com.internetms52.object_pool.scanner;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import org.apache.maven.plugin.logging.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Optional;
+
+public class ClassAnnotationScanner {
+    Log logger;
+    String annotationName;
+
+
+    public ClassAnnotationScanner(Log logger, String annotationName) {
+        this.logger = logger;
+        this.annotationName = annotationName;
+    }
+
+    public Optional<ClassAnnotationScanResponse> execute(File javaFile) {
+        Optional<ClassAnnotationScanResponse> resultOpt = Optional.empty();
+        try (FileInputStream fileInputStream = new FileInputStream(javaFile)) {
+            CompilationUnit cu = StaticJavaParser.parse(fileInputStream);
+            // 遍歷AST，查找特定annotation
+            boolean haveAnnotation =
+                    cu.findAll(ClassOrInterfaceDeclaration.class).stream().anyMatch(classDeclaration -> {
+                        return classDeclaration.isAnnotationPresent(annotationName);
+                    });
+            if (haveAnnotation) {
+                resultOpt = Optional.of(
+                        new ClassAnnotationScanResponse(
+                                javaFile.getPath()
+                        )
+                );
+            }
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+        }
+        return resultOpt;
+    }
+}
