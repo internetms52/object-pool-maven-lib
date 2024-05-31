c533d63d58bb63e140c5f9c49fb177e0f401dfab
internetms52 <internetms52@gmail.com>
2024-05-31 22:30:43 +0800

    update

---
+++ maven-plugin/src/main/java/com/internetms52/object_pool/annotation/processor/ObjectPoolProcessor.java
@@ -0,0 +1,49 @@
+package com.internetms52.object_pool.annotation.processor;
+
+import com.google.auto.service.AutoService;
+import com.internetms52.object_pool.annotation.ObjectPool;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+
+@AutoService(Processor.class)
+@SupportedAnnotationTypes("com.internetms52.object_pool.annotation.ObjectPool")
+//@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class ObjectPoolProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        System.out.println("ObjectPoolProcessor");
+        for (Element element : roundEnv.getElementsAnnotatedWith(ObjectPool.class)) {
+            if (element instanceof TypeElement) {
+                TypeElement classElement = (TypeElement) element;
+                String packageName = processingEnv.getElementUtils().getPackageOf(classElement).getQualifiedName().toString();
+                String className = classElement.getSimpleName().toString();
+
+                TypeSpec generatedClass = TypeSpec.classBuilder(className + "Generated")
+                        .addModifiers(Modifier.PUBLIC)
+                        .build();
+
+                JavaFile javaFile = JavaFile.builder(packageName, generatedClass)
+                        .build();
+                try {
+                    Path generatedSources = Paths.get("target/generated-sources");
+                    javaFile.writeTo(generatedSources);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return true;
+    }
+}
--- maven-plugin/src/test/java/com/internetms52/object_pool/AppTest.java
+++ maven-plugin/src/test/java/com/internetms52/object_pool/AppTest.java
@@ -1,12 +1,14 @@
-package com.internetms52.lombok_auto_testing;
+package com.internetms52.object_pool;
 
 import static org.junit.Assert.assertTrue;
 
+import com.internetms52.object_pool.annotation.ObjectPool;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
+@ObjectPool
 public class AppTest 
 {
     /**
