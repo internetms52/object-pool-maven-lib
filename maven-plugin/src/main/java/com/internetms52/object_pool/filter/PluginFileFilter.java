3044e35eba3ae86c627de049472af2623c9556f4
internetms52 <internetms52@gmail.com>
2024-06-01 15:18:20 +0800

    simplify

--- maven-plugin/src/main/java/com/internetms52/object_pool/filter/PluginFileFilter.java
+++
@@ -1,5 +0,0 @@
-package com.internetms52.object_pool.filter;
-
-public interface PluginFileFilter<T> {
-    boolean filter(T file);
-}
--- maven-plugin/src/main/java/com/internetms52/object_pool/scanner/PluginFileScanner.java
+++
@@ -1,7 +0,0 @@
-package com.internetms52.object_pool.scanner;
-
-import java.util.List;
-
-public interface PluginFileScanner<T> {
-    public List<T> execute(String rootPath);
-}
