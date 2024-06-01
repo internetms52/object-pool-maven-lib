f253987e7d2255455824c69e966caa87e6d03902
internetms52 <internetms52@gmail.com>
2024-06-01 16:15:52 +0800

    update

--- maven-lib/src/main/java/com/internetms52/object_pool/getter/ObjectPoolGetterNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.internetms52.object_pool.getter;
-
-public class ObjectPoolGetterNotFoundException extends Exception {
-    public ObjectPoolGetterNotFoundException(String message) {
-        super(message);
-    }
-}
